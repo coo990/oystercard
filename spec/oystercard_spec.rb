bcf55da9530d94a052875c7e91d2755be99b59ca
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-02 14:24:14 +0100

    Added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it 'it has zero balance when brand new' do
     expect(subject.balance).to eq(0)
   end
+  
+  describe '#top_up' do
+    it 'add money to oystercard' do
+      expect(subject.top_up(50)).to eq(50)
+    end
+  end
 end
